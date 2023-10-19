7dd14f6c52bdf10ab19165f67ce9396d656feaa3
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2023-10-19 10:08:32 -0400

    Update auto-language.js

--- auto-language.js
+++ auto-language.js
@@ -3,8 +3,8 @@ var tracks = player.textTracks();
 for (var i = 0; i < tracks.length; i++) {
   var track = tracks[i];
 
-  // Find the English captions track and mark it as "showing".
-  if (track.kind === 'captions' && track.language === 'en') {
-    track.mode = 'hidden';
+  // 
+  if (track.kind === 'captions') {
+    track.mode = 'disabled';
   }
 }
