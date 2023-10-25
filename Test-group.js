dcfaed4c312f1795881447656514e404913e01f5
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2023-10-25 15:00:36 -0400

    Update Test-group.js

--- Test-group.js
+++ Test-group.js
@@ -1,31 +1,79 @@
-var MenuButton = videojs.getComponent("MenuButton");
-var MenuItem = videojs.getComponent("MenuItem");
+const MenuItem = videojs.getComponent("MenuItem");
+const MenuButton = videojs.getComponent("MenuButton");
 
-// New syntax
 class CustomMenuButton extends MenuButton {
   createItems() {
-    // Must return an array of `MenuItem`s
-    // Options passed in `addChild` are available at `this.options_`
-    return this.options_.myItems.map((i) => {
-      var item = new MenuItem(this.player_, { label: i.name });
-      item.handleClick = function () {
-        /* ... */
-      };
-      return item;
+    const items = [];
+    const { myItems } = this.options_;
+
+    if (!Array.isArray(myItems)) items;
+
+    myItems.forEach(({ clickHandler, ...item }) => {
+      const menuItem = new MenuItem(this.player(), item);
+
+      if (clickHandler) {
+        //This overrides https://github.com/videojs/video.js/blob/main/src/js/menu/menu-item.js#L107-L109
+        menuItem.handleClick = clickHandler;
+      }
+      items.push(menuItem);
     });
+
+    return items;
   }
 
   buildCSSClass() {
-    return `vjs-chapters-button ${super.buildCSSClass()}`; //Add an icon to your menu
+    return `${super.buildCSSClass()}`;
   }
 }
 
-// Register as a component, so it can be added
 videojs.registerComponent("CustomMenuButton", CustomMenuButton);
 
-// Use `addChild` to add an instance of the new component, with options
-player.controlBar.addChild("CustomMenuButton", {
-  controlText: "My Awesome Menu", //It shows a text when the menu is hovered
-  title: "My menu",
-  myItems: [{ name: "Hello" }, { name: "World" }],
+var player = videojs("my-player", {
+  controlBar: {
+    //customMenuButton uses the ability of videojs to do type inference
+    customMenuButton: {
+      controlText: "Custom menu 1",
+      title: "Menu 1",
+      className: "vjs-chapters-button", //uses the chapters icon
+      myItems: [
+        {
+          label: "Pink",
+          clickHandler() {
+            this.el().style = "background-color:pink";
+          },
+        },
+        {
+          label: "Green",
+          clickHandler() {
+            this.el().style = "background-color:green";
+          },
+        },
+      ],
+    },
+    //If you want a specific name for your CustomMenuButton instance
+    consoleLoggerMenu: {
+      componentClass: "CustomMenuButton", //Defines the component Class to use
+      controlText: "Custom menu 2",
+      title: "Menu 2",
+      className: "vjs-mute-control",  //uses the volume icon
+      myItems: [
+        {
+          label: "Menu item 1",
+          clickHandler() {
+            console.log(this.label);
+          },
+        },
+        {
+          label: "Menu item 2",
+          clickHandler() {
+            console.log(this.label);
+          },
+        },
+      ],
+    },
+  },
 });
+
+//Now you can access you custom menu instance
+player.controlBar.customMenuButton;
+player.controlBar.consoleLoggerMenu;
